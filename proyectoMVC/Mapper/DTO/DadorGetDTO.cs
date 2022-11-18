e0d8998536062ee8b1a1701931bfdd36671e3643
Santiago Martinez <79525736+MartinezSantiago@users.noreply.github.com>
2022-11-18 13:16:50 -0300

    Add files via upload

---
+++ proyectoMVC/Mapper/DTO/DadorGetDTO.cs
@@ -0,0 +1,22 @@
+﻿using proyectoMVC.Helper;
+using System.ComponentModel.DataAnnotations;
+
+namespace proyectoMVC.Mapper.DTO
+{
+    public class DadorGetDTO
+    {
+        public int Id { get; set; }
+ 
+        public string Localidad { get; set; }
+     
+        public string Direccion { get; set; }
+        
+        public string ImagenPersona { get; set; }
+     
+
+        public string FrenteDNI { get; set; }
+       
+
+        public string DorsoDNI { get; set; }
+    }
+}
