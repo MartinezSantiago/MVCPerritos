0ee6376b9b761e0618383d8444b642d9755193d3
Santiago Martinez <79525736+MartinezSantiago@users.noreply.github.com>
2022-11-18 13:17:31 -0300

    Add files via upload

--- proyectoMVC/Services/DadorService.cs
+++ proyectoMVC/Services/DadorService.cs
@@ -1,4 +1,6 @@
-﻿using proyectoMVC.Helper;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using proyectoMVC.Helper;
 using proyectoMVC.Mapper;
 using proyectoMVC.Mapper.DTO;
 using AppContext = proyectoMVC.Context.AppContext;
@@ -13,14 +15,14 @@ namespace proyectoMVC.Services
         {
             this.context = context;
             this.autoMapper = autoMapper;
-           
+
             this.imageToDirectory = imageToDirectory;
         }
 
         public async Task Post(DadorPostDTO dadorInfoPostDTO, IWebHostEnvironment webHostEnvironment, int UserId)
         {
-            var pathImagenUsuario =  imageToDirectory.UploadImageToDirectoy(dadorInfoPostDTO.ImagenPersona, webHostEnvironment);
-            var pathFrenteDNI =  imageToDirectory.UploadImageToDirectoy(dadorInfoPostDTO.FrenteDNI, webHostEnvironment);
+            var pathImagenUsuario = imageToDirectory.UploadImageToDirectoy(dadorInfoPostDTO.ImagenPersona, webHostEnvironment);
+            var pathFrenteDNI = imageToDirectory.UploadImageToDirectoy(dadorInfoPostDTO.FrenteDNI, webHostEnvironment);
             var pathDorsoDNI = imageToDirectory.UploadImageToDirectoy(dadorInfoPostDTO.DorsoDNI, webHostEnvironment);
 
 
@@ -28,6 +30,61 @@ namespace proyectoMVC.Services
             context.dadores.Add(dador);
             await context.SaveChangesAsync();
         }
+        public async Task<List<DadorGetDTO>> Get()
+        {
+            var dadores = await context.dadores.Where(x => x.Revisado != true && x.IsDeleted == false).ToListAsync();
+            List<DadorGetDTO> dadorGetDTOs = new List<DadorGetDTO>();
+            foreach (var dador in dadores)
+            {
+                dadorGetDTOs.Add(autoMapper.DadorToDadorGetDTO(dador));
 
+            }
+            return dadorGetDTOs;
+        }
+        public async Task<bool> Autorizar(int Id)
+        {
+            var dador = await context.dadores.Where(x => x.Id == Id).FirstOrDefaultAsync();
+            if (dador != null)
+            {
+
+
+
+
+                try
+                {
+                    var user = await context.users.Where(x => x.Id == dador.UserId).FirstOrDefaultAsync();
+                    dador.Revisado = true;
+                    dador.EsCorrecto = true; 
+                    dador.LastUpdate = DateTime.Now;
+                    user.role = "dador";
+                    context.dadores.Update(dador);
+                    context.users.Update(user);
+                    await context.SaveChangesAsync();
+                    return true;
+                }
+                catch (Exception)
+                {
+
+                    return false;
+                }
+
+            }
+            return false;
+
+        }
+        public async Task<bool> Denegar(int Id)
+        {
+            var dador = await context.dadores.Where(x => x.Id == Id).FirstOrDefaultAsync();
+            if (dador != null)
+            { 
+                dador.Revisado = true;
+                dador.EsCorrecto = false;
+                context.dadores.Update(dador);
+                await context.SaveChangesAsync();
+            }
+            return false;
+
+        }
     }
+  
 }
